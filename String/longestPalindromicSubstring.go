8c2c2978c2db1992ee72c6f03532a1b6d8e20946
zwxhenu <zangliterature@163.com>
2022-03-14 18:40:43 +0800

    update

--- String/longestPalindromicSubstring.go
+++ String/longestPalindromicSubstring.go
@@ -25,14 +25,50 @@ s 仅由数字和英文字母组成
  * 边界问题字符在大于2之上。一个字符串或者两个字符串相同则为回文串且j-i+1的最大值就是我们所求的长字符串
  */
 func LongestPalindrome(s string) string {
-	var StrLen = len(s)
-	if StrLen < 2 {
+	if len(s) < 2 {
 		return s
 	}
-	// for i := 0; i < StrLen; i++ {
 
+	// dp[i][j]表示字符串s[i,j]是否为回文串
+	dp := make([][]bool, len(s))
+
+	//初始化列
+	for i, _ := range dp {
+		dp[i] = make([]bool, len(s))
+	}
+	/*
+		  4  e           *
+		  3  d        *  *
+		  2  c     *  *  *
+		  1  b  *  *  *  *
+		  0  a  b  c  d  e
+			 0	1  2  3  4
+	*/
+	start := 0  //最大回文串的开始下标
+	maxLen := 0 //最大回文串的长度-1(最后再+1就可以表示长度,不要在循环里面每次都+1)
+	// i要从大到小
+	for i := len(s) - 1; i >= 0; i-- {
+		// j要从小到大
+		for j := i; j < len(s); j++ {
+			length := j - i
+			// 可以写成一行 dp[i][j] = s[i] == s[j] && (length <= 2 || dp[i+1][j-1])
+			if s[i] == s[j] {
+				if length <= 2 {
+					dp[i][j] = true
+				} else {
+					dp[i][j] = dp[i+1][j-1]
+				}
+			}
+			if dp[i][j] && length > maxLen {
+				maxLen = length
+				start = i
+			}
+		}
+	}
+	// for i, v := range dp {
+	// 	fmt.Printf("dp[%d]=%+v\n", i, v)
 	// }
-	return s
+	return s[start : start+maxLen+1]
 }
 
 /**
